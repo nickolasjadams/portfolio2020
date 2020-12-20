fd89871297764d117ee7180351a7976425edbbee
Nick Adams <nickolas.j.adams@gmail.com>
2020-12-19 22:14:15 -0700

    Templated in pug and added scripts.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Portfolio Template
+
+Fork the repository
+```bash
+git clone whatever
+```
+
+You'll have a portfolio ready for hosting in no time!
+Copy your resume PDF into the /pdf directory.
+Copy your own logo to `/images/logo.png`
+
+Run the setup shell script
+```bash
+./setup
+```
+This will prompt you for some important information.
+This information will be saved to `prod.json` and the portfolio will be compiled.
+
+The information in `prod.json` will be injected into the pug templates. 
+
+Any changes to the templates or sass after this will need to be recompiled. This can be done in one step with 
+```bash
+npm run build
+```
+
+Places you may want to modify content
+ - Home Page `pug/index.pug`
+ - The SEO tag in `pug/index.pug` and `pug/resume.pug`
+ - Navigation `pug/_partials/_header.pug`
+
--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,19 @@ if (localStorage.getItem("darkMode")) {
 }
 
 document.querySelector(".dark-mode").addEventListener("click", function() {
+	$(".toast").removeClass("d-none");
 	if (bodyClasses.toString().indexOf("dark") > -1) {
 		bodyClasses.remove("dark");
 		localStorage.clear();
+		$(".toast-body").html("Woah! The lighter is my favorite too!<br>I'll remember that for you.");
 	} else {
 		bodyClasses.add("dark");
 		localStorage.setItem("darkMode", true);
+		$(".toast-body").html("That's much easier on the eyes. I like dark much more.<br>I'll remember that for you.");
 	}
+	var d = new Date();
+	var hour = (d.getHours() > 12) ? d.getHours() % 12 : d.getHours();
+	var minutes = (d.getMinutes() < 10) ? "0" + d.getMinutes() : d.getMinutes();
+	$('.toast .timestamp').html(hour + ":" + minutes);
+	$('.toast').toast('show');
 });
