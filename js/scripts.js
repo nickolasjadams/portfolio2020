2158475c0c114af17678152db63d41e5d96d96e7
Nick Adams <nickolas.j.adams@gmail.com>
2020-12-26 02:52:56 -0700

    block is a reserved keyword in mixins bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,20 @@ document.querySelector(".dark-mode").addEventListener("click", function() {
 	$('.toast .timestamp').html(hour + ":" + minutes);
 	$('.toast').toast('show');
 });
+
+var slug = window.location.pathname;
+var domain = window.location.hostname;
+
+if (slug == "/" || slug.indexOf("index.html") > -1) {
+	$("nav a:contains('Home')").addClass('active');
+} else {
+	document.querySelectorAll('nav a').forEach(function(a) {
+		// must contain domain
+		if (a.href.indexOf(domain) > -1) {
+			// and slug
+			if (a.href.indexOf(slug) > -1) {
+				a.classList.add('active');
+			}
+		}
+	});
+}
